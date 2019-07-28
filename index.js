49e27137f6dbdf90c968202ee40209d051776219
Ng Ying Kai <yingkai96@outlook.com>
2019-07-28 20:29:31 +0800

    added comments for better understanding

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// Use Express for NodeJS
 const express = require('express');
 
 // setup express app
 const app = express();
 
-app.get('/', function(req,res){
+// localhost:4000/api
+app.get('/api', function(req,res){
     console.log('GET request');
-    res.end();
+    // Send string as response
+    res.send({ name: 'Yoshi'});
 })
 
 // listen for requests
 app.listen(4000, function(){
+    // Log string to console
     console.log('now listening for requests')
 });
